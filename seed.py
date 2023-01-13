160ad949c743bf89e48921def543d02754a3a074
beegeiger <beegeiger@gmail.com>
2023-01-12 16:45:54 -0800

    Setting up seed file

--- seed.py
+++ seed.py
@@ -1,4 +1,14 @@
 import requests
 from secrets import GM_API_KEY
 
-uri_base = "https://maps.googleapis.com/maps/api/place/findplacefromtext/"
+uri_base1 = "https://maps.googleapis.com/maps/api/place/findplacefromtext/"
+uri_base2 = "https://maps.googleapis.com/maps/api/place/textsearch/"
+
+endpoint = "json?query=restaurants%20in%20Sydney&key=" + GM_API_KEY
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
