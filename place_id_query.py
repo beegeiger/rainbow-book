14e98ceec86d8ef5d91afe0e5f32c761716705f2
beegeiger <beegeiger@gmail.com>
2023-01-17 13:57:04 -0800

    Made place_id query into a function

--- place_id_query.py
+++ place_id_query.py
@@ -5,14 +5,15 @@ uri_place_details = "https://maps.googleapis.com/maps/api/place/details/json?pla
 
 place_id = "ChIJt_cTCjWHj4ARXGHUHr_Jt6Y"
 
-payload = {}
-headers = {}
-
-url = uri_place_details + place_id + GM_API_KEY
-
-response = requests.request("GET", url, headers=headers, data=payload)
 
 print(url)
 print(response.text)
 with open("place_from_id1.txt", "w") as f:
     f.write(response.text)
+
+def gm_id_query(place_id):
+    payload = {}
+    headers = {}
+    url = uri_place_details + place_id + GM_API_KEY
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response
