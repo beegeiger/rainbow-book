8ff2f3fc974f7b090a870d66a7d17510cc04d33a
beegeiger <beegeiger@gmail.com>
2023-01-20 15:05:18 -0800

    Created id and place lookup paths to location paths blueprint

--- Components/location_paths.py
+++ Components/location_paths.py
@@ -5,7 +5,20 @@ from place_name_query import gm_place_query
 locations_rb = Blueprint('locations_rb', __name__, template_folder='../Static/templates',
     static_folder='../Static')
 
-@locations_rb("/loc_lookup", methods=["GET"])
-def find_loc(place_uri):
+@locations_rb("/loc_id_lookup", methods=["GET"])
+def find_place_id(place_uri):
     """Takes a Google Maps Place URI and looks up the google place_id"""
-    return place_id
+    gm_response = gm_place_query(place_uri)
+    print(response)
+    print(response.text)
+    print(response.text.place_id)
+    return response.text
+
+@locations_rb("/place_lookup", methods=["GET"])
+def loc_lookup(place_id):
+    """Takes a Google Maps Place URI and looks up the google place_id"""
+    gm_response = gm_id_query(place_id)
+    print(response)
+    print(response.text)
+    print(response.text.place_id)
+    return response.text
