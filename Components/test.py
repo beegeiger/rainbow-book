c39e2ff1841936e6c62357a6a2db2c87d64e4280
beegeiger <beegeiger@gmail.com>
2023-01-25 16:21:16 -0800

    Place name query function now outputs the place id

---
+++ Components/test.py
@@ -0,0 +1,5 @@
+import requests
+# from place_id_query import gm_id_query
+from place_name_query import gm_place_query
+
+print(gm_place_query("https://www.google.com/maps/place/Alameda+County+Superior+Courthouse/@37.7969823,-122.2585934,16z/data=!4m8!1m2!3m1!2sAlameda+County+Superior+Courthouse!3m4!1s0x808f87350a13f7b7:0xa6b7c9bf1ed4615c!8m2!3d37.7996116!4d-122.2631207"))
