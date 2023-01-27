eb813b191357268fe7dc3fd51e5c9624d2033d9a
beegeiger <beegeiger@gmail.com>
2023-01-26 17:46:58 -0800

    Added helper functions script

---
+++ Components/helper_functions.py
@@ -0,0 +1,13 @@
+import requests
+from place_id_query import gm_id_query
+from place_name_query import gm_place_query
+
+def get_id_from_response(response):
+    """Takes response and outputs place_id"""
+    ind_loc = response.text.find("place_id")
+    ind_lis1 = response.text[ind_loc:].split(":")
+    ind_lis2 = ind_lis1[1].split(",")
+    ind_lis3 = ind_lis2[0]
+    return ind_lis3
+
+def check_place_in_db()
